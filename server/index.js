0bd8eee23fa9bee266ec2be97d94fb80af362a76
PizdAKluvv <tamerlanross@gmail.com>
2025-03-12 19:07:56 +0200

    push14

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     cors({
-        origin: ['http://localhost:5173', 'https://node-js-ovqm.onrender.com'],
+        origin: ['http://localhost:5173', 'https://kapilka-0l7y.onrender.com'],
         credentials:true,
     })
 );
