5067332b9641164e8c3a9f0163fa6b5605bb9419
PizdAKluvv <tamerlanross@gmail.com>
2025-03-12 18:51:33 +0200

    push11

--- server/index.js
+++ server/index.js
@@ -1,28 +1,24 @@
 const express = require('express');
-const router = require('./routes');
-const connectDB = require('./lib/connect');
-const cookieParser = require('cookie-parser'); 
+const router = require("./routes");
+const connectDb = require('./lib/connect.js');
+const cookieParser =require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
-app.use(cookieParser());
 app.use(express.json());
-
-// localhost:3000/api/sign-in
-// localhost:3000/api/add-income
-
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  cors({  
-  origin: ['http://localhost:5173', 'https://node-js-ovqm.onrender.com'],
-    credentials: true,
-  })
-)
-
-
-app.use('/api',router);
+    cors({
+        origin: ['http://localhost:5173','http://localhost:5174', 'https://node-js-ovqm.onrender.com'],
+        credentials:true,
+    })
+);
+app.use('/api',router)
+console.log(process.env.DB_URL)
 
 app.listen(3000, () => {
-  connectDB();
-  console.log('Server is running on http://localhost:3000');
+    connectDb();
+    console.log('server is running on http://localhost:3000');
 });
-
